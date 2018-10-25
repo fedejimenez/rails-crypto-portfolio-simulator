5bba53abf9d029116173a65c11167c8707be6109
Federico Jimenez <fedej_mdp@hotmail.com>
2018-10-25 08:43:54 +0800

    Revert "Feature/crypto api"

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
-  # Homepage
-  root 'home#index'
-  get 'home/about'
-  get 'home/lookup'
-  post 'home/lookup' => 'home/lookup'
-
-
-  # Signup. The first renders a form, the second receives the form and create a user in the database.
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-
-  # these routes are for showing users a login form and log in / out.
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20181024083931_add_columns_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :firstname, :string
-    add_column :users, :lastname, :string
-    add_column :users, :verification, :boolean
-    add_column :users, :role, :string, default: "user"
-    add_column :users, :avatar, :string
-  end
-end
