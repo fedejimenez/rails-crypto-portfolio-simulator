b41598c16343dd9c817ab9f15b52f596bc22ac58
fedejimenez <fedej_mdp@hotmail.com>
2018-11-06 22:00:52 -0300

    Add css to forms

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,15 +30,3 @@ $(document).on('turbolinks:load', function() {
 	document.querySelector('#q').parentNode.classList.add("lookup-input")
 })
 
-// Google Places autocomplete (Search Bar)
-var autocomplete;
-
-function initAutocomplete() {
-	// Create the autocomplete object
-	autocomplete = new google.maps.places.Autocomplete(
-	    /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-	    {types: ['geocode']});
-	autocomplete.addListener('place_changed',  function() {
-	// var place = autocomplete.getPlace();
-  });
-}
