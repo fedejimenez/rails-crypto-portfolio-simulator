dc12fdb768e571f2868f0865bbcd774da0639c56
Federico Jimenez <fedeejimenez@gmail.com>
2020-03-12 19:56:26 +0000

    Update API from CoinMarket and use CryptoCompare for historical
    CoinMarket Updated its API https://coinmarketcap.com/api/documentation/v1/
    and historical prices are not available for free anymore

--- app/models/search.rb
+++ app/models/search.rb
@@ -9,16 +9,16 @@ class Search < ApplicationRecord
     # @coins_filtered = @coins.select {|c| c["symbol"] == symbol.upcase } if symbol.present?
     if min_price.present? && max_price.present?
     	@coins_filtered = @coins.select {
-    		|c| (c["price_usd"].to_d > min_price.to_d && c["price_usd"].to_d < max_price.to_d)
+    		|c| (c['quote']['USD']['price'].to_d > min_price.to_d && c['quote']['USD']['price'].to_d < max_price.to_d)
     	} 
     else 
-    	@coins_filtered = @coins.select {|c| c["price_usd"].to_d > min_price.to_d } if min_price.present?
-    	@coins_filtered = @coins.select {|c| c["price_usd"].to_d < max_price.to_d } if max_price.present?
+    	@coins_filtered = @coins.select {|c| c['quote']['USD']['price'].to_d > min_price.to_d } if min_price.present?
+    	@coins_filtered = @coins.select {|c| c['quote']['USD']['price'].to_d < max_price.to_d } if max_price.present?
     end
 
-  	@coins_filtered = @coins.select {|c| c["percent_change_1h"].to_d.round(2) > percent_change_1h.to_d.round(2) } if percent_change_1h.present?
-  	@coins_filtered = @coins.select {|c| c["percent_change_24h"].to_d.round(2) > percent_change_24h.to_d.round(2) } if percent_change_24h.present?
-  	@coins_filtered = @coins.select {|c| c["percent_change_7d"].to_d.round(2) > percent_change_7d.to_d.round(2) } if percent_change_7d.present?
+  	@coins_filtered = @coins.select {|c| c['quote']['USD']['percent_change_1h'].to_d.round(2) > percent_change_1h.to_d.round(2) } if percent_change_1h.present?
+  	@coins_filtered = @coins.select {|c| c['quote']['USD']['percent_change_24h'].to_d.round(2) > percent_change_24h.to_d.round(2) } if percent_change_24h.present?
+  	@coins_filtered = @coins.select {|c| c['quote']['USD']['percent_change_7d'].to_d.round(2) > percent_change_7d.to_d.round(2) } if percent_change_7d.present?
 
     return @coins_filtered
 	end
